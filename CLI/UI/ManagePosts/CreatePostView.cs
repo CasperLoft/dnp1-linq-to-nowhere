920ea29a0c1a5a1fe32f75184d14c037cd67ae64
Casper Loft Elgaard <128655301+Xorgh@users.noreply.github.com>
2026-09-13 16:39:49 +0200

    Enhance CreatePostView by adding user validation and repository integration

--- CLI/UI/ManagePosts/CreatePostView.cs
+++ CLI/UI/ManagePosts/CreatePostView.cs
@@ -6,21 +6,39 @@ namespace CLI.UI.ManagePosts;
 public class CreatePostView
 {
     private readonly IPostRepository _postRepository;
+    private readonly IUserRepository _userRepository;
 
-    public CreatePostView(IPostRepository postRepository)
+
+    public CreatePostView(IPostRepository postRepository, IUserRepository userRepository)
     {
         this._postRepository = postRepository;
+        this._userRepository = userRepository;
     }
 
     public async Task StartAsync()
     {
-        int userId;
+        IQueryable<User> users = _userRepository.GetManyAsync();
 
-        while (!int.TryParse(
-                   CliApp.ReadRequiredInput("Enter your user ID: "),
-                   out userId))
+        int userId;
+        
+        while (true)
         {
-            Console.WriteLine("User ID must be a number.");
+            string input = CliApp.ReadRequiredInput(
+                "Enter your user ID: ");
+
+            if (!int.TryParse(input, out userId))
+            {
+                Console.WriteLine("User ID must be a number.");
+                continue;
+            }
+
+            if (!users.Any(user => user.Id == userId))
+            {
+                Console.WriteLine("No user exists with that ID.");
+                continue;
+            }
+
+            break;
         }
 
         string password = CliApp.ReadRequiredInput("Enter your password: ");
@@ -35,9 +53,9 @@ public class CreatePostView
         };
 
         Post createdPost = await _postRepository.AddAsync(newPost);
-        
+
         Console.WriteLine($"Post created with ID: {createdPost.Id}");
-        
+
         Console.WriteLine();
         Console.WriteLine("Press any key to return...");
         Console.ReadKey();
